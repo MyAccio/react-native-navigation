af2047fffc4900b8030011817a1e010fc98c042d
Paul Demchuk <pademchuk@gmail.com>
2018-03-21 11:07:30 -0700

    Update SingleScreenLayout.java

--- android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
@@ -267,7 +267,7 @@ public class SingleScreenLayout extends BaseLayout {
     @Override
     public void dismissLightBox() {
         if (lightBox != null) {
-            lightBox.hide();
+            lightBox.dismiss();
             lightBox = null;
         }
     }
